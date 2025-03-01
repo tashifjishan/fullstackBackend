598a3728f5c438bff7a3aa79a4944078c3b60129
pradhan <kpradhan2933@gmail.com>
2025-03-01 14:51:06 +0530

    hello

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 app.get("/", (req, res)=>{
-    res.status(200).json({status: "OK", message: "Welcome!"})
+    try {
+        res.status(200).json({status: "OK", message: "Welcome!"})
+    } catch (error) {
+        res.status(500).json({status: "ERROR", message: "Internal Server Error"})
+    }
 })
 app.post("/login", (req, res) => {
     try {
